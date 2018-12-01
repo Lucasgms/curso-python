efe31571466252c96cb4b0f55ca87fb94dbf5dad
Lucasgms <lgregory12@hotmail.com>
2018-11-30 23:15:52 -0200

    [WIP] Exercícios da Aula 2 - Atividade 5 do Curso Python 3.
    Alteracao para desconsiderar espacamento e text transform do input do usuário na hora da comparacao com a palavra secreta.

--- forca.py
+++ forca.py
@@ -11,10 +11,11 @@ def jogar():
     while not enforcou and not acertou:
 
         chute = input("Qual letra?")
+        chute = chute.strip()
 
         index = 0
         for letra in palavra_secreta:
-            if(letra == chute):
+            if(chute.upper() == letra.upper()):
                 print("Encontrei a letra {} na posicao {} ".format(chute, index))
             index = index + 1
 
